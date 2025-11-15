59631ea36c0062d34a06d82927d02ca5b17e039b
virlpp <138609022+virlpp@users.noreply.github.com>
2025-11-14 23:43:44 -0300

    Update TestConexion.java

--- TPI_UsuarioCredencial/src/main/TestConexion.java
+++ TPI_UsuarioCredencial/src/main/TestConexion.java
@@ -1,1 +1,2 @@
-
+// Archivo utilitario. No es parte del flujo de la aplicación.
+// La prueba fue realizada dentro del main de DatabaseConnection.java. TestConexion cumple exactamente la misma función
