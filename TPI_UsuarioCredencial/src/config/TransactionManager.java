553213db2f4e9ebc59cb4a7539e038b3c333191c
virlpp <138609022+virlpp@users.noreply.github.com>
2025-11-14 21:59:54 -0300

    Create TransactionManager.java

---
+++ TPI_UsuarioCredencial/src/config/TransactionManager.java
@@ -0,0 +1,2 @@
+// Gestiona la atomicidad de las operaciones de la base de datos. Su único trabajo es hacer cumplir la regla de "Todo o Nada".
+// En este proyecto, se usará para operaciones como crear un Usuario y su Credencial al mismo tiempo.
